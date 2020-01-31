47f01acd0ab42891d1a008e4b00db57634f3a0eb
owenanderson <owenda7@gmail.com>
2020-01-31 16:22:16 -0500

    start front end and parser

--- parser.py
+++ parser.py
@@ -1,9 +1,24 @@
+from Tree import *
+
 def parse(search):
-    search_array = []
+    array = []
     for c in search:
         word = ""
         if c == " ":
-            search_array.append(word)
-            word = ""
+            if word in ["of","by"]:
+                word = ""
+            else:
+                array.append(word)
+                word = ""
         else:
             word += c
+    return array
+
+def buildTree(array):
+    if array[0]:
+    newTree = Tree(array[0])
+    array.remove[0]
+    newTree.setRightChild(buildTree)
+
+
+
