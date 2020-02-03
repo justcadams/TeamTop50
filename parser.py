6e450e0cf8eb41330fd1a55f6097f951355c57df
owenanderson <owenda7@gmail.com>
2020-02-03 18:39:24 -0500

    start front end and parser

--- parser.py
+++ parser.py
@@ -1,12 +1,15 @@
 from Tree import *
 
 BINARY_KEYWORDS = ["compare", "more"]
+BUZZWORDS = ["of", "by"]
+
+
 def parse(search):
     array = []
     for c in search:
         word = ""
         if c == " ":
-            if word in ["of","by"]:
+            if word in BUZZWORDS:
                 word = ""
             else:
                 array.append(word)
@@ -15,20 +18,40 @@ def parse(search):
             word += c
     return array
 
+
 def buildTree(array):
-    if array[0] != BINARY_KEYWORDS:
-        Tree newTree = Tree(array[0])
-        array.remove[0]
+    # check if first item in array is unary keyword
+    if array[0] not in BINARY_KEYWORDS:
+        newTree = Tree(array[0])
+        array.pop(0)
         newTree.setRightChild(buildTree(array))
-    elif array[0] == ["compare"]
-        Tree newTree = Tree(array[0]+array[1])
-        array.remove[0]
-        array.remove[1]
-        count = 0
-        index = 0
-        while (array[index] != "and") and (count != 0):
-            if array[index] == BINARY_KEYWORDS:
-                count ++
-            if array
+
+    # run else if binary keyword detected as first element in array
+    else:
+        newTree = Tree(array[0]+array[1])
+        array.pop(0)
+        array.pop(1)
+
+        newArray = []   # newArray created to hold all arguments before and identifier
+        count = 0       # count variable used to keep track of encapsulated binary arguments
+        index = 0       # index used to loop through array
+
+        ### TODO: Exception handling for when no "and" is detected and index exceeds size of array
+        # While loop splits the first half of arguments before the and identifier and remaining arguments after
+        # into two arrays (newArray and array)
+        while (array[index] == "and") and (count != 0):
+            if array[index] in BINARY_KEYWORDS:
+                count += 1
+                newArray.append(array.pop(0))
+            elif (array[index] == "and"):
+                count -= 1
+                newArray.append(array.pop(0))
+            else:
+                newArray.append(array.pop(0))
+            index += 1
+
+        # build left and right branches with the two arrays
+        newTree.setLeftChild(buildTree(newArray))
+        newTree.setRightChild(buildTree(array))
 
 
