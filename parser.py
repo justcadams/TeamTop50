39b8819d1ab017c1457d1aab1f0564cd6a9f00ae
owenanderson <owenda7@gmail.com>
2020-02-03 17:46:38 -0500

    start front end and parser

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 from Tree import *
 
+BINARY_KEYWORDS = ["compare", "more"]
 def parse(search):
     array = []
     for c in search:
@@ -15,10 +16,19 @@ def parse(search):
     return array
 
 def buildTree(array):
-    if array[0]:
-    newTree = Tree(array[0])
-    array.remove[0]
-    newTree.setRightChild(buildTree)
-
+    if array[0] != BINARY_KEYWORDS:
+        Tree newTree = Tree(array[0])
+        array.remove[0]
+        newTree.setRightChild(buildTree(array))
+    elif array[0] == ["compare"]
+        Tree newTree = Tree(array[0]+array[1])
+        array.remove[0]
+        array.remove[1]
+        count = 0
+        index = 0
+        while (array[index] != "and") and (count != 0):
+            if array[index] == BINARY_KEYWORDS:
+                count ++
+            if array
 
 
