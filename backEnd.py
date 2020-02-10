912d17888ab612b94d94d515c35252fa089f0545
Justin Adams <justin.adams@uvm.edu>
2020-02-10 10:47:33 -0500

    Resolving code conflicts between local and origin.

--- backEnd.py
+++ backEnd.py
@@ -4,14 +4,14 @@ import sqlite3
 import psutil
 import regex
 import urllib3
-import Tkinter as tk
+import tkinter as tk
 import pandas as pd
-import tkFileDialog as filedialog
+from tkinter import filedialog
 from sqlite3 import Error
 
 class SQLBackEnd:
 	# databaseConnection contains a tuple with the sqlite3 connection object first and the filename string object second.
-	def __init__(self,filename):
+	def __init__(self, filename):
 		# Field to maintain multiple connections.
 		self.databaseConnections = list()
 		# Field to identify the current server this terminal is interfacing with.
@@ -42,7 +42,7 @@ class SQLBackEnd:
 			# If there is an error print out the error to the log.
 			print(e)
 
-	def changeConnection(self,connectionNumber):
+	def changeConnection(self, connectionNumber):
 		# Ensure the connection this terminal is changing to is within the range of available server connections.
 		if(connectionNumber > -1 and connectionNumber < len(self.databaseConnections)):
 			# Update the current connection to reflect this terminal's selection.
@@ -59,7 +59,7 @@ class SQLBackEnd:
 	def displayCurrentConnection(self):
 		print("Connection " + str(self.currentConnection) + " is selected.")
 	
-	def disconnectFromServer(self,listLocation):
+	def disconnectFromServer(self, listLocation):
 		sqlite3.disconnect(self.databaseConnection[listLocation][1])
 
 	def createDatabase(self, databaseName, filename):
@@ -131,7 +131,7 @@ class SQLBackEnd:
 					# Print database connection status.
 					print(newConnection.sqlite3_status())
 					# Store connection and filename for later reference.
-					databaseConnection.append(tuple((newConnection,filename)))
+					databaseConnections.append(tuple((newConnection,filename)))
 				except Error as e:
 					print(e)
 				finally:
@@ -149,7 +149,7 @@ class SQLBackEnd:
 					# Print database connection status.
 					print(newConnection.sqlite3_status())
 					# Store connection and filename for later reference.
-					databaseConnection.append(tuple((newConnection,filename)))
+					self.databaseConnections.append(tuple((newConnection,filename)))
 				except Error as e:
 					print(e)
 				finally:
@@ -163,25 +163,25 @@ class SQLBackEnd:
 		filePath = filedialog.askopenfilename()
 		songs = pd.read_csv(filePath)
 		# dtypes = {'ID': 'INTEGER', 'Track.Name': 'str', 'Artist.Name': 'str', 'Genre': 'str', 'Beats.Per.Minute': 'INTEGER', 'Energy': 'INTEGER', 'Danceability': 'INTEGER', 'Loudness': 'INTEGER', 'Liveness': 'INTEGER', 'Valence': 'INTEGER', 'Length': 'INTEGER', 'Acousticness': 'INTEGER', 'Speechiness': 'INTEGER', 'Popularity': 'INTEGER'}
-		songs.to_sql('TOP50', self.currentConnection, if_exists='append', index = False)
+		songs.to_sql('TOP50', self.currentConnection, if_exists='append', index=False)
 		self.currentConnection.commit()
 	
-	def deleteDatabase(self,databaseName):
-		if(regexCheck(databaseName)):
+	def deleteDatabase(self, databaseName):
+		if(self.regexCheck(databaseName)):
 			print("Hacking attempt detected. Ignoring user input.")
 		else:
 			self.currentTerminal.execute("DROP " + databaseName)
 			self.currentTerminal.commit()
 
 	def deleteTable(self, tableName):
-		if(regexCheck(databaseName)):
+		if(self.regexCheck(tableName)):
 			print("Hacking attempt detected. Ignoring user input.")
 		else:
 			self.currentTerminal.execute("DROP TABLE " + tableName)
 			self.currentTerminal.commit()
 
 	def deleteColumn(self, tableName, columnNames):
-		if(regexCheck(databaseName)):
+		if(self.regexCheck(tableName + columnNames)):
 			print("Hacking attempt detected. Ignoring user input.")
 		else:
 			SQLCommand = "ALTER TABLE " + tableName + " DROP COLUMN "
@@ -194,9 +194,8 @@ class SQLBackEnd:
 			self.currentTerminal.commit()
 
 
-	def createTable(self,tableName):
-		regexCheck = False
-		if(regexCheck(tableName)):
+	def createTable(self, tableName):
+		if(self.regexCheck(tableName)):
 			print("Hacking attempt detected. Ignoring user input.")
 		else:
 			if(type(tableName) == str):
@@ -205,7 +204,7 @@ class SQLBackEnd:
 			else:
 				print("Table name is not a valid type.")
 
-	def createTable(self,tableName,columnNames,columnDataTypes):
+	def createTable(self, tableName, columnNames, columnDataTypes):
 		regexCheck = False
 		if(regexCheck(tableName + columnnNames + columnDataTypes)):
 			print("Hacking attempt detected. Ignoring user input.")
@@ -219,7 +218,7 @@ class SQLBackEnd:
 			self.currentTerminal.execute(SQLString)
 			self.currentTerminal.commit()
 
-	def createRows(self,tableName,columnnNames,rowData):
+	def createRows(self, tableName, columnnNames, rowData):
 		if(regexCheck(tableName + columnnNames + rowData)):
 			print("Hacking attempt detected. Ignoring user input.")
 		else:
@@ -229,6 +228,8 @@ class SQLBackEnd:
 		databaseString = self.currentTerminal.execute("SELECT * FROM " + tableName)
 		print(databaseString)
 
+	def regexCheck(self):
+		return False
 
 # Justin's workspace
 
