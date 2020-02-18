e03e02234ee13d2a4a021c1c1ac9be7256310483
Matt Halligan <halligansventures@gmail.com>
2020-02-18 11:57:09 -0500

    Initialization of the app is in place, needs css and interface with parser via put method

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 from flask import Flask
+from flask import Blueprint, render_template, request
+
+
+
+
 
 app = Flask(__name__)
+main = Blueprint("main", __name__)
+
+@app.route('/', methods=['PUT'])
+def index():
+
+    data = [['text', "First"], ['text', "Second"], ['text', "Third"]]
+    return render_template("index.html", data=data)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=["PUT"])
+def submission():
+
+    data = []
+    return render_template("index.html", data=data)
+
 
 
 if __name__ == '__main__':
+
     app.run()
+
+
